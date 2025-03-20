c69e3c4b6ac5bf9538714a39e4ee815516ec233e
Kaksloup <abdoulaye.kake.pro@gmail.com>
2025-03-20 22:11:31 +0100

    Ajout du nouveau output de test

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
     setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
     collectCoverage: true,
     coverageDirectory: ".coverage",
-    coverageReporters: ["json", "lcov", "text", "clover"],
+    coverageReporters: ["json", "lcov", "text", "clover", "cobertura"],
     testTimeout: 30000,
     coveragePathIgnorePatterns: ["/node_modules/", "/.coverage/"],
     reporters: ["default", ["jest-junit", { outputDirectory: ".coverage", outputName: "junit.xml" }]]
